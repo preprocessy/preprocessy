131df3e8206d92e00f78b678d89d88095599e2b4
Priyesh1202 <priyesh.vakharia@gmail.com>
2021-01-10 16:16:51 +0530

    Added folder for handling outliers

---
+++ preprocessy/outliers/handleoutlier.py
@@ -0,0 +1,100 @@
+class HandleOutlier:
+
+
+    """ Class for handling outliers on its own or according to users needs.
+
+        Private methods
+       _ _ _ _ _ _ _ _ _ _
+
+       __returnQuartiles() : returns the 5% and 95% mark in the distrubution 
+                             of data(Values above are default values)
+
+        Public Methods
+       _ _ _ _ _ _ _ _ _
+
+       handleOutliers() : Takes in the dataset as input, finds the quartiles
+                          and returns the dataset within the interquartile 
+                          range. Function run only on int64 and float64 
+                          specified columns.
+
+    """
+
+
+    def __init__(self, train_df, params):
+
+
+        """ Function to initialize a few parameters used to make the process
+            run without human interaction.
+
+            Parameters to be entered by the user include :
+                -The dataset
+                -cols: Any specific columns in dataset the user wants to 
+                       remove outliers from. If not entered columns will be
+                       selected based on their dtype(int and float only) and 
+                       outliers will be removed from them
+                -removeoutliers : default = True
+                -replace : default = False => if user wants to replace 
+                           outliers with -999 everywhere instead of 
+                           removing them
+                -q1 : specify the starting range (Default is 0.05)
+                -q3 : specify the end of the range (Default is 0.95)
+
+        """
+        self.train_df = train_df
+        self.cols = []
+        self.removeoutliers = True
+        self.replace = False
+        self.quartiles = {}
+        self.qone = 0.05
+        self.qthree = 0.95
+        if 'cols' in params.keys():
+            self.cols = params['cols']
+        if 'removeoutliers' in params.keys():
+            self.removeoutliers = params['removeoutliers']
+        if 'replace' in params.keys():
+            self.replace = params['replace'] 
+        if 'q1' in params.keys():
+            self.qone = params['q1']
+        if 'q3' in params.keys():
+            self.qthree = params['q3']
+
+    def __return_quartiles(self,col):
+        # return the quartile range or q1 and q3 values for the column passed as parameter
+        quartiles = self.quartiles
+        train_df = self.train_df
+        q1 = train_df[col].quantile(self.qone)
+        q1 = round(q1)
+        q3 = train_df[col].quantile(self.qthree)
+        q3 = round(q3)
+        quartiles[col] = [q1,q3]
+        return True
+
+    def handle_outliers(self):
+
+        ### parameters till now: train_df, cols, removeoutliers, replace
+        train_df = self.train_df
+        if self.removeoutliers: # if user has marked removeoutliers = True and wants outliers removed..
+            if len(self.cols)>=1:
+                for col in self.cols:
+                    self.__return_quartiles(col)
+                for col in self.cols:
+                    q = self.quartiles[col]
+                    q1 = q[0]
+                    q3 = q[1]
+                    train_df = train_df[(train_df[col]>=q1)]
+                    train_df = train_df[(train_df[col]<=q3)]
+
+        # if removeoutliers = False and replace=True ie user wants outliers replaced by a value to indicate these
+        # are outliers
+        elif self.replace: 
+            if len(self.cols)>=1:
+                for col in self.cols:
+                    self.__return_quartiles(col)
+                for col in self.cols:
+                    q = self.quartiles[col]
+                    q1 = q[0]
+                    q3 = q[1]
+                    train_df[(train_df[col]<q1)] = -999
+                    train_df[(train_df[col]>q3)] = -999
+        self.train_df = train_df
+        return self.train_df
