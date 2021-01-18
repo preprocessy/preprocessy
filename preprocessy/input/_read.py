1eaf43f76db6c003b23b9106950f171bc24928b8
Saif807380 <saif1204kazi@gmail.com>
2021-01-18 20:07:09 +0530

    Fixed input build fail and test cases

--- preprocessy/input/_read.py
+++ preprocessy/input/_read.py
@@ -1,41 +1,40 @@
 import pandas as pd
 import os
+import errno
 
 class ReadData(object):
     def __init__(self, file_name):
         self.excel_extensions = ["xls", "xlsx", "xlsm", "xlsb", "odf", "ods", "odt"]
         self.file_name = file_name
-        self.df = self.__read_file()
+        self.__read_file()
         self.summary, self.stats = self.__read_summary()
-        
+
     def __read_file(self):
         """Read the file content"""
-        try:
-            self.df = None
-            if ".csv" in self.file_name:
-                self.df = pd.read_csv(self.file_name, index_col=0)
-                if self.df is None:
-                    self.df = pd.read_csv(self.file_name, index_col=0, delimiter=";")
-            elif ".tsv" in self.file_name:
-                self.df = pd.read_csv(self.file_name, sep="\t")
-            elif self.file_name.split(".")[-1] in self.excel_extensions:
-                self.df = pd.read_excel(self.file_name)
-            else:
-                raise ValueError(
-                    f'Unsupported filetype. Supported extensions include [.csv, .tsv, .xls, .xlsx, .xlsm, .xlsb, .odf, .ods and .odt]. Received file of type .{self.file_name.split(".")[-1]}'
-                )
 
+        self.df = None
+
+        if ".csv" in self.file_name:
+            self.df = pd.read_csv(self.file_name, index_col=0)
+            if self.df is None:
+                self.df = pd.read_csv(self.file_name, index_col=0, delimiter=";")
+        elif ".tsv" in self.file_name:
+            self.df = pd.read_csv(self.file_name, sep="\t")
+        elif self.file_name.split(".")[-1] in self.excel_extensions:
+            self.df = pd.read_excel(self.file_name)
+        else:
+            raise ValueError(
+                f'Unsupported filetype. Supported extensions include [.csv, .tsv, .xls, .xlsx, .xlsm, .xlsb, .odf, .ods and .odt]. Received file of type .{self.file_name.split(".")[-1]}'
+            )
+
+        if self.df is not None:
             self.df.drop(
                 self.df.columns[self.df.columns.str.contains("unnamed", case=False)],
                 axis=1,
                 inplace=True,
             )
-
-        except FileNotFoundError:
-            print(f"{self.file_name} not found")
-
-        finally:
-            return self.df
+        else:
+            raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), self.file_name)
 
     def __read_summary(self):
         """Read file summary"""
--- tests/test_input.py
+++ tests/test_input.py
@@ -14,3 +14,7 @@ class TestReader:
         assert isinstance(reader.df, pd.core.frame.DataFrame) == True
         assert reader.df.shape == (3, 4)
         assert reader.stats.iloc[1, 0] == -4003
+
+    def test_file_not_exists(self):
+        with pytest.raises(FileNotFoundError):
+            reader = ReadData(file_name="hello.csv")
