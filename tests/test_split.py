438df10a20f8d5500f7bcb292efb0d00ae7932bd
Bhumika-Kothwal <kothwalbhumika@gmail.com>
2021-03-12 19:43:02 +0530

    updated test_split.py to use pytest parameterization

--- tests/test_split.py
+++ tests/test_split.py
@@ -5,34 +5,34 @@ import pytest
 from preprocessy.resampling import Split
 
 
-class TestSplitting:
-    def test_without_target_col(self):
-        df = pd.DataFrame(np.arange(1000).reshape(100, 10))
-        params = {"X": df, "test_size": 0.2, "random_state": 420}
+
+def test_without_target_col():
+    df = pd.DataFrame(np.arange(1000).reshape(100, 10))
+    params = {"X": df, "test_size": 0.2, "random_state": 420}
+    split = Split()
+    split.train_test_split(params=params)
+    assert params["train"].shape[0] == 80
+    assert params["train"].shape[1] == 10
+    assert params["test"].shape[0] == 20
+    assert params["test"].shape[1] == 10
+
+def test_random_state():
+    df = pd.DataFrame(np.arange(1000).reshape(100, 10))
+    # test passes if function raises a TypeError
+    with pytest.raises(TypeError):
+        params = {"X": df, "test_size": 0.2, "random_state": "hello"}
         split = Split()
         split.train_test_split(params=params)
-        assert params["train"].shape[0] == 80
-        assert params["train"].shape[1] == 10
-        assert params["test"].shape[0] == 20
-        assert params["test"].shape[1] == 10
 
-    def test_random_state(self):
-        df = pd.DataFrame(np.arange(1000).reshape(100, 10))
-        # test passes if function raises a TypeError
-        with pytest.raises(TypeError):
-            params = {"X": df, "test_size": 0.2, "random_state": "hello"}
-            split = Split()
-            split.train_test_split(params=params)
-
-    def test_without_target_label(self):
-        df_x = pd.DataFrame(np.arange(1000).reshape(100, 10))
-        df_y = pd.Series(np.arange(100))
-        with pytest.raises(ValueError):
-            params = {
-                "X": df_x,
-                "y": df_y,
-                "test_size": 0.2,
-                "random_state": 420,
-            }
-            split = Split()
-            split.train_test_split(params=params)
+def test_without_target_label():
+    df_x = pd.DataFrame(np.arange(1000).reshape(100, 10))
+    df_y = pd.Series(np.arange(100))
+    with pytest.raises(ValueError):
+        params = {
+            "X": df_x,
+            "y": df_y,
+            "test_size": 0.2,
+            "random_state": 420,
+        }
+        split = Split()
+        split.train_test_split(params=params)
