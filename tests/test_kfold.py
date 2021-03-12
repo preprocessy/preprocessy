a7903a84b62af4787e53295bf2c3943134b648e9
Bhumika-Kothwal <kothwalbhumika@gmail.com>
2021-03-12 19:39:13 +0530

    updated test_kfold.py to use pytest parameterization

--- tests/test_kfold.py
+++ tests/test_kfold.py
@@ -4,36 +4,35 @@ import pytest
 from preprocessy.resampling import KFold
 
 
-class TestKFold:
-    def test_kfold(self):
-        with pytest.raises(ValueError):
-            KFold(n_splits=2.3)
+@pytest.mark.parametrize(
+    "test_input1, test_input2, test_input3",
+    [
+        (2.3, False, None),
+        (0, False, None),
+        (5, 1, None),
+        (5, True, 4.5),
+        (5, False, 69),
+    ],
+)
+def test_kfold(test_input1, test_input2, test_input3):
+    with pytest.raises(ValueError):
+        KFold(
+            n_splits=test_input1, shuffle=test_input2, random_state=test_input3
+        )
 
-        with pytest.raises(ValueError):
-            KFold(n_splits=0)
 
-        with pytest.raises(ValueError):
-            KFold(shuffle=1)
-
-        with pytest.raises(ValueError):
-            KFold(shuffle=True, random_state=4.5)
-
-        with pytest.raises(ValueError):
-            KFold(shuffle=False, random_state=69)
-
-    def test_split(self):
-
-        with pytest.raises(ValueError):
-            arr = np.arange(10)
-            kFold = KFold(n_splits=20)
-            for train_indices, test_indices in kFold.split(arr):
-                print(
-                    f"Train indices: {train_indices}\nTest indices:"
-                    f" {test_indices}"
-                )
-
-        arr = np.arange(12)
-        kFold = KFold(n_splits=3, shuffle=True, random_state=69)
+def test_split():
+    with pytest.raises(ValueError):
+        arr = np.arange(10)
+        kFold = KFold(n_splits=20)
         for train_indices, test_indices in kFold.split(arr):
-            assert len(train_indices) == 8
-            assert len(test_indices) == 4
+            print(
+                f"Train indices: {train_indices}\nTest indices:"
+                f" {test_indices}"
+            )
+
+    arr = np.arange(12)
+    kFold = KFold(n_splits=3, shuffle=True, random_state=69)
+    for train_indices, test_indices in kFold.split(arr):
+        assert len(train_indices) == 8
+        assert len(test_indices) == 4
