f81ab9c4bd2456b9c7d794422909d6c87721625a
Bhumika-Kothwal <kothwalbhumika@gmail.com>
2021-03-12 19:43:02 +0530

    updated test_split.py to use pytest parameterization

--- tests/test_split.py
+++ tests/test_split.py
@@ -7,19 +7,19 @@ from preprocessy.resampling import Split
 split = Split()
 
 
-class TestSplitting:
-    def test_without_target_label(self):
-        df = pd.DataFrame(np.arange(1000).reshape(100, 10))
-        params = {"X": df, "test_size": 0.2, "random_state": 420}
-        split.train_test_split(params=params)
-        assert params["train"].shape[0] == 80
-        assert params["train"].shape[1] == 10
-        assert params["test"].shape[0] == 20
-        assert params["test"].shape[1] == 10
+def test_without_target_label():
+    df = pd.DataFrame(np.arange(1000).reshape(100, 10))
+    params = {"X": df, "test_size": 0.2, "random_state": 420}
+    split.train_test_split(params=params)
+    assert params["train"].shape[0] == 80
+    assert params["train"].shape[1] == 10
+    assert params["test"].shape[0] == 20
+    assert params["test"].shape[1] == 10
 
-    def test_random_state(self):
-        df = pd.DataFrame(np.arange(1000).reshape(100, 10))
-        # test passes if function raises a TypeError
-        with pytest.raises(TypeError):
-            params = {"X": df, "test_size": 0.2, "random_state": "hello"}
-            split.train_test_split(params=params)
+
+def test_random_state():
+    df = pd.DataFrame(np.arange(1000).reshape(100, 10))
+    # test passes if function raises a TypeError
+    with pytest.raises(TypeError):
+        params = {"X": df, "test_size": 0.2, "random_state": "hello"}
+        split.train_test_split(params=params)
