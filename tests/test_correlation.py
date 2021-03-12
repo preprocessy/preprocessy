1d080835425645f50010a4f9cdfd2e708c9a7f78
Bhumika-Kothwal <kothwalbhumika@gmail.com>
2021-03-12 19:39:33 +0530

    updated test_correlation.py to use pytest parameterization

--- tests/test_correlation.py
+++ tests/test_correlation.py
@@ -6,24 +6,26 @@ from preprocessy.feature_selection import Correlation
 X, y = load_iris(return_X_y=True, as_frame=True)
 
 
-class TestCorrelation:
-    def test_with_empty_df(self):
-        with pytest.raises(ValueError):
-            for tup in Correlation().find(None):
-                print(tup)
+def test_with_empty_df():
+    with pytest.raises(ValueError):
+        for tup in Correlation().find(None):
+            print(tup)
 
-    def test_with_invalid_threshold(self):
-        with pytest.raises(ValueError):
-            for tup in Correlation().find(X, 20):
-                print(tup)
 
-    def test_with_threshold(self):
-        count = 0
-        for _ in Correlation().find(X, threshold=0.9):
-            count += 1
-        assert count == 1
+def test_with_invalid_threshold():
+    with pytest.raises(ValueError):
+        for tup in Correlation().find(X, 20):
+            print(tup)
 
-    def test_for_same_column(self):
-        # A column is highly correlated with itself. Test to check that this correlation is not included.
-        for tup in Correlation().find(X):
-            assert tup[0] != tup[1]
+
+def test_with_threshold():
+    count = 0
+    for _ in Correlation().find(X, threshold=0.9):
+        count += 1
+    assert count == 1
+
+
+def test_for_same_column():
+    # A column is highly correlated with itself. Test to check that this correlation is not included.
+    for tup in Correlation().find(X):
+        assert tup[0] != tup[1]
