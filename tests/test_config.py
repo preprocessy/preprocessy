6be8a2de762593b2b12994cc07ceca7bf4dfaf12
Bhumika-Kothwal <kothwalbhumika@gmail.com>
2021-03-14 11:16:35 +0530

    updated test_config.py to use pytest parameterization

--- tests/test_config.py
+++ tests/test_config.py
@@ -4,38 +4,41 @@ from preprocessy.pipelines.config import read_config
 from preprocessy.pipelines.config import save_config
 
 
-class TestConfig:
-    def test_filenotfound(self):
-        with pytest.raises(FileNotFoundError):
-            _ = read_config("/usr/src/app/config.json")
+def test_filenotfound():
+    with pytest.raises(FileNotFoundError):
+        _ = read_config("/usr/src/app/config.json")
 
-    def test_jsonload(self):
-        with pytest.raises(TypeError):
-            _ = read_config("./datasets/configs/config2.json")
 
-    def test_df(self):
-        with pytest.raises(FileNotFoundError):
-            _ = read_config("./datasets/configs/config_df.json")
+def test_jsonload():
+    with pytest.raises(TypeError):
+        _ = read_config("./datasets/configs/config2.json")
 
-    def test_read(self):
-        params = read_config("./datasets/configs/config4.json")
-        exp = {
-            "param1": 69,
-            "param2": {"nestedParam": 420},
-            "Split": 6969,
-            "df": "./datasets/encoding/test.csv",
-        }
-        assert params == exp
 
-    def test_save(self):
-        filepath = "./datasets/configs/params.json"
-        params = {
-            "param1": 69,
-            "param2": {"nestedParam": 420},
-            "Split": 6969,
-            "df": "./datasets/encoding/test.csv",
-        }
-        save_config(filepath, params)
-        contents = read_config(filepath)
-        params.pop("df")
-        assert params == contents
+def test_df():
+    with pytest.raises(FileNotFoundError):
+        _ = read_config("./datasets/configs/config_df.json")
+
+
+def test_read():
+    params = read_config("./datasets/configs/config4.json")
+    exp = {
+        "param1": 69,
+        "param2": {"nestedParam": 420},
+        "Split": 6969,
+        "df": "./datasets/encoding/test.csv",
+    }
+    assert params == exp
+
+
+def test_save():
+    filepath = "./datasets/configs/params.json"
+    params = {
+        "param1": 69,
+        "param2": {"nestedParam": 420},
+        "Split": 6969,
+        "df": "./datasets/encoding/test.csv",
+    }
+    save_config(filepath, params)
+    contents = read_config(filepath)
+    params.pop("df")
+    assert params == contents
