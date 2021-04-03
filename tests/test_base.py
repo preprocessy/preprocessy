375eb39deee27469a85a807b52d0d8681ee799da
Bhumika-Kothwal <64397046+Bhumika-Kothwal@users.noreply.github.com>
2021-04-03 22:44:54 +0530

    Pytest parameterization (#54)
    Parametrized tests and moved tests out from classes.

--- tests/test_base.py
+++ tests/test_base.py
@@ -31,163 +31,185 @@ def split(params):
     ]
 
 
-class TestBasePipeline:
-    def test_pipeline_arguments(self):
+@pytest.mark.parametrize(
+    "error, df_path, steps, config_file, params, custom_reader",
+    [
+        (ArgumentsError, None, None, None, None, None),
+        (
+            ArgumentsError,
+            None,
+            [custom_read, times_two, squared, split],
+            None,
+            None,
+            None,
+        ),
+        (
+            TypeError,
+            "./datasets/configs/dataset.csv",
+            [custom_read, "times_two", squared, split],
+            None,
+            ["hello"],
+            None,
+        ),
+        (
+            TypeError,
+            "./datasets/configs/dataset.csv",
+            [custom_read, times_two, squared, split],
+            None,
+            ["hello"],
+            None,
+        ),
+        (
+            TypeError,
+            "./datasets/configs/dataset.csv",
+            [times_two, squared, split],
+            None,
+            {"col_1": "A"},
+            "custom_read",
+        ),
+    ],
+)
+def test_pipeline_arguments(
+    error, df_path, steps, config_file, params, custom_reader
+):
 
-        with pytest.raises(ArgumentsError):
-            Pipeline()
-
-        with pytest.raises(ArgumentsError):
-            Pipeline(steps=[custom_read, times_two, squared, split])
-
-        with pytest.raises(TypeError):
-            Pipeline(
-                df_path="./datasets/configs/dataset.csv",
-                steps=[custom_read, "times_two", squared, split],
-                params=["hello"],
-            )
-
-        with pytest.raises(TypeError):
-            Pipeline(
-                df_path="./datasets/configs/dataset.csv",
-                steps=[custom_read, times_two, squared, split],
-                params=["hello"],
-            )
-
-        with pytest.raises(TypeError):
-            Pipeline(
-                df_path="./datasets/configs/dataset.csv",
-                steps=[times_two, squared, split],
-                params={"col_1": "A"},
-                custom_reader="custom_read",
-            )
-
-    def test_pipeline_with_default_reader(self):
-        df = pd.DataFrame({"A": np.arange(1, 100), "B": np.arange(1, 100)})
-        _ = df.to_csv("./datasets/configs/dataset.csv", index=False)
-
-        params = {
-            "col_1": "A",
-            "col_2": "B",
-            "test_size": 0.2,
-        }
-
-        pipeline = Pipeline(
-            df_path="./datasets/configs/dataset.csv",
-            steps=[times_two, squared, split],
+    with pytest.raises(error):
+        Pipeline(
+            df_path=df_path,
+            steps=steps,
+            config_file=config_file,
             params=params,
-        )
-        pipeline.process()
-
-        assert "df" in pipeline.params.keys()
-        assert "summary" in pipeline.params.keys()
-        assert "stats" in pipeline.params.keys()
-
-    def test_pipeline_with_custom_reader(self):
-
-        df = pd.DataFrame({"A": np.arange(1, 100), "B": np.arange(1, 100)})
-        _ = df.to_csv("./datasets/configs/dataset.csv", index=False)
-
-        params = {
-            "col_1": "A",
-            "col_2": "B",
-            "test_size": 0.2,
-            "df": "./datasets/configs/dataset.csv",
-        }
-
-        pipeline = Pipeline(
-            df_path="./datasets/configs/dataset.csv",
-            steps=[times_two, squared, split],
-            params=params,
-            custom_reader=custom_read,
-        )
-        pipeline.process()
-
-        assert (
-            pipeline.params["df"].loc[69, "A"]
-            == pipeline.params["df_copy"].loc[69, "A"] * 2
-        )
-        assert (
-            pipeline.params["df"].loc[42, "B"]
-            == pipeline.params["df_copy"].loc[42, "B"] ** 2
+            custom_reader=custom_reader,
         )
 
-        assert len(pipeline.params["X_train"]) == 80
 
-    def test_add(self):
-        df = pd.DataFrame({"A": np.arange(1, 100), "B": np.arange(1, 100)})
-        _ = df.to_csv("./datasets/configs/dataset.csv", index=False)
-        params = {
-            "col_1": "A",
-            "test_size": 0.2,
-        }
-        pipeline = Pipeline(
-            df_path="./datasets/configs/dataset.csv",
-            steps=[times_two, split],
-            params=params,
-        )
-        pipeline.process()
-        assert pipeline.params["df"].loc[42, "A"] == df.loc[42, "A"] * 2
-        pipeline.add(
-            squared,
-            {
-                "col_2": "A",
-            },
-            before="times_two",
-        )
-        pipeline.process()
-        num_0 = pipeline.params["df"].loc[42, "A"]
-        num_1 = df.loc[42, "A"]
-        assert num_0 == (num_1 ** 2) * 2
-        pipeline.remove("squared")
-        pipeline.add(squared, {"col_2": "A"}, after="read_file")
-        pipeline.process()
-        num_0 = pipeline.params["df"].loc[42, "A"]
-        num_1 = df.loc[42, "A"]
-        assert num_0 == (num_1 ** 2) * 2
+def test_pipeline_with_default_reader():
+    df = pd.DataFrame({"A": np.arange(1, 100), "B": np.arange(1, 100)})
+    _ = df.to_csv("./datasets/configs/dataset.csv", index=False)
 
-    def test_remove(self):
-        df = pd.DataFrame({"A": np.arange(1, 100), "B": np.arange(1, 100)})
-        _ = df.to_csv("./datasets/configs/dataset.csv", index=False)
-        params = {
-            "col_1": "A",
-            "col_2": "B",
-            "test_size": 0.2,
-        }
-        pipeline = Pipeline(
-            df_path="./datasets/configs/dataset.csv",
-            steps=[times_two, squared, split],
-            params=params,
-        )
-        pipeline.process()
-        assert len(pipeline.params["X_train"]) == 80
-        pipeline.remove("split")
-        pipeline.process()
-        assert pipeline.params["df"].shape[0] == df.shape[0]
+    params = {
+        "col_1": "A",
+        "col_2": "B",
+        "test_size": 0.2,
+    }
 
-    def test_config(self):
-        df = pd.DataFrame({"A": np.arange(1, 100), "B": np.arange(1, 100)})
-        _ = df.to_csv("./datasets/configs/dataset.csv", index=False)
-        params = {
-            "df": "./datasets/configs/dataset.csv",
-            "col_1": "A",
-            "col_2": "B",
-            "test_size": 0.2,
-        }
-        config_path = "./datasets/configs/pipeline_config.json"
-        save_config(config_path, params)
+    pipeline = Pipeline(
+        df_path="./datasets/configs/dataset.csv",
+        steps=[times_two, squared, split],
+        params=params,
+    )
+    pipeline.process()
 
-        pipeline = Pipeline(
-            df_path="./datasets/configs/dataset.csv",
-            steps=[times_two, squared, split],
-            config_file=config_path,
-            custom_reader=custom_read,
-        )
-        pipeline.process()
-        assert len(pipeline.params["X_train"]) == 80
-        pipeline.remove("split")
-        pipeline.process()
-        assert (
-            pipeline.params["df"].shape[0]
-            == pipeline.params["df_copy"].shape[0]
-        )
+    assert "df" in pipeline.params.keys()
+    assert "summary" in pipeline.params.keys()
+    assert "stats" in pipeline.params.keys()
+
+
+def test_pipeline_with_custom_reader():
+    df = pd.DataFrame({"A": np.arange(1, 100), "B": np.arange(1, 100)})
+    _ = df.to_csv("./datasets/configs/dataset.csv", index=False)
+
+    params = {
+        "col_1": "A",
+        "col_2": "B",
+        "test_size": 0.2,
+        "df": "./datasets/configs/dataset.csv",
+    }
+
+    pipeline = Pipeline(
+        df_path="./datasets/configs/dataset.csv",
+        steps=[times_two, squared, split],
+        params=params,
+        custom_reader=custom_read,
+    )
+    pipeline.process()
+
+    assert (
+        pipeline.params["df"].loc[69, "A"]
+        == pipeline.params["df_copy"].loc[69, "A"] * 2
+    )
+    assert (
+        pipeline.params["df"].loc[42, "B"]
+        == pipeline.params["df_copy"].loc[42, "B"] ** 2
+    )
+
+    assert len(pipeline.params["X_train"]) == 80
+
+
+def test_add():
+    df = pd.DataFrame({"A": np.arange(1, 100), "B": np.arange(1, 100)})
+    _ = df.to_csv("./datasets/configs/dataset.csv", index=False)
+    params = {
+        "col_1": "A",
+        "test_size": 0.2,
+    }
+    pipeline = Pipeline(
+        df_path="./datasets/configs/dataset.csv",
+        steps=[times_two, split],
+        params=params,
+    )
+    pipeline.process()
+    assert pipeline.params["df"].loc[42, "A"] == df.loc[42, "A"] * 2
+    pipeline.add(
+        squared,
+        {
+            "col_2": "A",
+        },
+        before="times_two",
+    )
+    pipeline.process()
+    num_0 = pipeline.params["df"].loc[42, "A"]
+    num_1 = df.loc[42, "A"]
+    assert num_0 == (num_1 ** 2) * 2
+    pipeline.remove("squared")
+    pipeline.add(squared, {"col_2": "A"}, after="read_file")
+    pipeline.process()
+    num_0 = pipeline.params["df"].loc[42, "A"]
+    num_1 = df.loc[42, "A"]
+    assert num_0 == (num_1 ** 2) * 2
+
+
+def test_remove():
+    df = pd.DataFrame({"A": np.arange(1, 100), "B": np.arange(1, 100)})
+    _ = df.to_csv("./datasets/configs/dataset.csv", index=False)
+    params = {
+        "col_1": "A",
+        "col_2": "B",
+        "test_size": 0.2,
+    }
+    pipeline = Pipeline(
+        df_path="./datasets/configs/dataset.csv",
+        steps=[times_two, squared, split],
+        params=params,
+    )
+    pipeline.process()
+    assert len(pipeline.params["X_train"]) == 80
+    pipeline.remove("split")
+    pipeline.process()
+    assert pipeline.params["df"].shape[0] == df.shape[0]
+
+
+def test_config():
+    df = pd.DataFrame({"A": np.arange(1, 100), "B": np.arange(1, 100)})
+    _ = df.to_csv("./datasets/configs/dataset.csv", index=False)
+    params = {
+        "df": "./datasets/configs/dataset.csv",
+        "col_1": "A",
+        "col_2": "B",
+        "test_size": 0.2,
+    }
+    config_path = "./datasets/configs/pipeline_config.json"
+    save_config(config_path, params)
+    pipeline = Pipeline(
+        df_path="./datasets/configs/dataset.csv",
+        steps=[times_two, squared, split],
+        config_file=config_path,
+        custom_reader=custom_read,
+    )
+    pipeline.process()
+    assert len(pipeline.params["X_train"]) == 80
+    pipeline.remove("split")
+    pipeline.process()
+    assert (
+        pipeline.params["df"].shape[0] == pipeline.params["df_copy"].shape[0]
+    )
--- tests/test_input.py
+++ tests/test_input.py
@@ -6,24 +6,27 @@ from preprocessy.input import ReadData
 reader = ReadData()
 
 
-class TestReader:
-    def test_incorrect_file_name_type(self):
-        with pytest.raises(TypeError):
-            reader.read_file({"df_path": None})
+@pytest.mark.parametrize(
+    "test_input",
+    [{"df_path": None}, {"df_path": ["datasets/encoding/test.csv"]}],
+)
+def test_incorrect_file_name_type(test_input):
+    with pytest.raises(TypeError):
+        reader.read_file(params=test_input)
 
-        with pytest.raises(TypeError):
-            reader.read_file({"df_path": ["datasets/encoding/test.csv"]})
 
-    def test_incorrect_file_type(self):
-        with pytest.raises(ValueError):
-            reader.read_file({"df_path": "datasets"})
+def test_incorrect_file_type():
+    with pytest.raises(ValueError):
+        reader.read_file({"df_path": "datasets"})
 
-    def test_reader(self):
-        reader.read_file({"df_path": "datasets/encoding/test.csv"})
-        assert isinstance(reader.df, pd.core.frame.DataFrame)
-        assert reader.df.shape == (3, 5)
-        assert reader.stats.iloc[1, 0] == -4003
 
-    def test_file_not_exists(self):
-        with pytest.raises(FileNotFoundError):
-            reader.read_file({"df_path": "hello.csv"})
+def test_reader():
+    reader.read_file({"df_path": "datasets/encoding/test.csv"})
+    assert isinstance(reader.df, pd.core.frame.DataFrame)
+    assert reader.df.shape == (3, 5)
+    assert reader.stats.iloc[1, 0] == -4003
+
+
+def test_file_not_exists():
+    with pytest.raises(FileNotFoundError):
+        reader.read_file({"df_path": "hello.csv"})
