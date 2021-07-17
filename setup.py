8ce2b480c0e4b0bbbb03dd4d326f5ceee906d8a7
Saif Kazi <50794619+Saif807380@users.noreply.github.com>
2021-07-17 12:21:35 +0530

    Added progress bar (#84)

--- setup.py
+++ setup.py
@@ -27,8 +27,12 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
+        "alive-progress>=1.6.2",
+        "colorama>=0.4.4",
         "pandas>=1.0.5",
-        "scikit-learn>=0.23.2"
+        "prettytable>=2.1.0",
+        "scikit-learn>=0.23.2",
+        "stringcase>=1.2.0"
         # add contents from requirements.txt only
         # "sample_package>=version_number"
     ],
