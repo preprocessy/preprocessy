d43b3931ff7f1e6dcbcffd5796529f17db4a8d3b
Saif807380 <saif1204kazi@gmail.com>
2021-08-13 21:29:30 +0530

    refactored kfold cross validation to support params

--- tests/test_kfold.py
+++ tests/test_kfold.py
@@ -1,10 +1,11 @@
 import numpy as np
+import pandas as pd
 import pytest
 from preprocessy.resampling import KFold
 
 
 @pytest.mark.parametrize(
-    "test_input1, test_input2, test_input3",
+    "n_splits, shuffle, random_state",
     [
         (2.3, False, None),
         (0, False, None),
@@ -13,25 +14,24 @@ from preprocessy.resampling import KFold
         (5, False, 69),
     ],
 )
-def test_kfold(test_input1, test_input2, test_input3):
+def test_kfold(n_splits, shuffle, random_state):
+    df = pd.DataFrame(np.arange(1000).reshape(100, 10))
     with pytest.raises(ValueError):
-        KFold(
-            n_splits=test_input1, shuffle=test_input2, random_state=test_input3
-        )
+        cv = KFold()
+        params = {
+            "train_df": df,
+            "n_splits": n_splits,
+            "shuffle": shuffle,
+            "random_state": random_state,
+        }
+        for _, _ in cv.split(params):
+            pass
 
 
 def test_split():
-    with pytest.raises(ValueError):
-        arr = np.arange(10)
-        kFold = KFold(n_splits=20)
-        for train_indices, test_indices in kFold.split(arr):
-            print(
-                f"Train indices: {train_indices}\nTest indices:"
-                f" {test_indices}"
-            )
-
-    arr = np.arange(12)
-    kFold = KFold(n_splits=3, shuffle=True, random_state=69)
-    for train_indices, test_indices in kFold.split(arr):
-        assert len(train_indices) == 8
-        assert len(test_indices) == 4
+    df = pd.DataFrame(np.arange(1000).reshape(100, 10))
+    cv = KFold()
+    params = {"train_df": df, "n_splits": 5, "shuffle": True}
+    for train_indices, test_indices in cv.split(params):
+        assert len(train_indices) == 80
+        assert len(test_indices) == 20
