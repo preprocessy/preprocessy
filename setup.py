7736e230f1ce9432e3703db0f3720b229f035158
Saif807380 <saif1204kazi@gmail.com>
2025-03-02 17:42:14 +0530

    Updated pandas version and min python version

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     install_requires=[
         "alive-progress~=1.6.2",
         "colorama>=0.4.4",
-        "pandas>=1.0.5",
+        "pandas>=2.2.3",
         "prettytable>=2.1.0",
         "scikit-learn>=1.0.0",
         "stringcase>=1.2.0"
